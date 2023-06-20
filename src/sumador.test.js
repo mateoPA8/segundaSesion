aa7ddaad77cc73e43e926049b56038f9ff425ef1
mateopoquechoquearnez <mateo.poquechoque@ucb.edu.bo>
2023-06-20 10:16:27 -0400

    devuelve correcto en caso de que se ingresen tamanos iguales de corcehets tanto abiertos como cerrados

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,7 +1,14 @@
+import calcularCorchetes from "./sumador.js";
 
-import calcularCorchetes from "./corchetes.js";
+
 describe("Sumar", () => {
   it("deberia de devolverme incorrecto si se ingresa solo [", () => {
     expect(calcularCorchetes("[")).toEqual("incorrecto");
+  });
+  it("deberia de devolverme incorrecto si se ingresa solo ]", () => {
+    expect(calcularCorchetes("]")).toEqual("incorrecto");
+  });
+  it("deberia de devolverme correcto si se ingresan []", () => {
+    expect(calcularCorchetes("[]")).toEqual("correcto");
   });
 });
