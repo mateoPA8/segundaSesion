8db30c1d0368b8810ad7a86557cfbde17cc9c2ca
mateopoquechoquearnez <mateo.poquechoque@ucb.edu.bo>
2023-06-20 10:36:11 -0400

    elimina las letras de la cadena si es que las hay

--- src/sumador.js
+++ src/sumador.js
@@ -1,16 +1,17 @@
 function calcularCorchetes(cadena) {
   var resultado="";
   var corchetes1=[];
+var cadenaSinNumeros=cadena.replace("0","").replace("1","").replace("2","").replace("3","").replace("4","").replace("5","").replace("6","").replace("7","").replace("8","").replace("9","").replace(/[a-z]/g,"");
+  //console.log(cadenaSinNumeros)
   var corchetes2=[];
-  if(cadena.length==1)
+  if(cadenaSinNumeros.length==1)
   {
-    if(cadena[0]=="[")
+    if(cadenaSinNumeros[0]=="[")
     {
-      console.log(cadena[0]);
       resultado="incorrecto";
       return resultado;
     }
-    if(cadena[0]=="]")
+    if(cadenaSinNumeros[0]=="]")
     {
       resultado="incorrecto";
       return resultado;
@@ -18,15 +19,15 @@ function calcularCorchetes(cadena) {
   }
   else
   {
-    for(let i=0;i<cadena.length;i++)
+    for(let i=0;i<cadenaSinNumeros.length;i++)
     {
-      if(cadena[i]=="[")
+      if(cadenaSinNumeros[i]=="[")
       {
-        corchetes1.push(cadena[i]);
+        corchetes1.push(cadenaSinNumeros[i]);
       }
-      if(cadena[i]=="]")
+      if(cadenaSinNumeros[i]=="]")
       {
-        corchetes2.push(cadena[i]);
+        corchetes2.push(cadenaSinNumeros[i]);
       }
     }
     if(corchetes1.length==corchetes2.length)
