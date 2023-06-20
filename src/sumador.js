38a3b298a4272335101550a386b1243c54c857fe
mateopoquechoquearnez <mateo.poquechoque@ucb.edu.bo>
2023-06-20 10:23:04 -0400

    elimina los numeros dentro de la cadena si es que los hay

--- src/sumador.js
+++ src/sumador.js
@@ -1,5 +1,42 @@
-function sumar(a, b) {
-  return a + b;
+function calcularCorchetes(cadena) {
+  var resultado="";
+  var corchetes1=[];
+  var corchetes2=[];
+  if(cadena.length==1)
+  {
+    if(cadena[0]=="[")
+    {
+      console.log(cadena[0]);
+      resultado="incorrecto";
+      return resultado;
+    }
+    if(cadena[0]=="]")
+    {
+      resultado="incorrecto";
+      return resultado;
+    }
+  }
+  else
+  {
+    for(let i=0;i<cadena.length;i++)
+    {
+      if(cadena[i]=="[")
+      {
+        corchetes1.push(cadena[i]);
+      }
+      if(cadena[i]=="]")
+      {
+        corchetes2.push(cadena[i]);
+      }
+    }
+    if(corchetes1.length==corchetes2.length)
+    {
+      resultado="correcto";
+      return resultado;
+    }
+  }
+  
+
 }
 
-export default sumar;
+export default calcularCorchetes;
